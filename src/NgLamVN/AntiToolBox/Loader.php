10a90232db6239474553a0f623ff87c27f51911c
Blackjack200 <lx826444126@gmail.com>
2021-07-01 20:37:57 +0800

    remove useless things

--- src/NgLamVN/AntiToolBox/Loader.php
+++ src/NgLamVN/AntiToolBox/Loader.php
@@ -7,61 +7,41 @@ namespace NgLamVN\AntiToolBox;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\protocol\types\DeviceOS;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class Loader extends PluginBase implements Listener
-{
-    public Config $config;
+class Loader extends PluginBase implements Listener {
+	public Config $config;
 
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-    }
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    /**
-     * @param DataPacketReceiveEvent $event
-     * @priority NORMAL
-     * @ignoreCancelled TRUE
-     */
-    public function onRecieve (DataPacketReceiveEvent $event)
-    {
-        $player = $event->getPlayer();
-        $packet = $event->getPacket();
+	/**
+	 * @param DataPacketReceiveEvent $event
+	 * @priority NORMAL
+	 * @ignoreCancelled true
+	 */
+	public function onReceive(DataPacketReceiveEvent $event) : void {
+		$player = $event->getPlayer();
+		$packet = $event->getPacket();
 
-        if ($packet instanceof LoginPacket)
-        {
-            $deviceOS = (int)$packet->clientData["DeviceOS"];
-            $deviceModel = (string)$packet->clientData["DeviceModel"];
-
-            if ($deviceOS !== 1) //AndroidOS
-            {
-                return;
-            }
-
-            /**
-             * Something about device model check, for example:
-             * Original client: XIAOMI Note 8 Pro
-             * Toolbox client: Xiaomi Note 8 Pro
-             *
-             * For another Example
-             * Original client: SAMSUNG SM-A105F
-             * Toolbox client: samsung SM-A105F
-             */
-
-            $name = explode(" ", $deviceModel);
-            if (!isset($name[0]))
-            {
-                return;
-            }
-            $check = $name[0];
-            $check = strtoupper($check);
-            if ($check !== $name[0])
-            {
-                $player->close("", $this->config->get("kick-message"));
-            }
-        }
-    }
+		if ($packet instanceof LoginPacket) {
+			$deviceOS = (int) ($packet->clientData['DeviceOS'] ?? -1);
+			$deviceModel = (string) ($packet->clientData['DeviceModel'] ?? '');
+			if ($deviceOS !== DeviceOS::ANDROID) {
+				return;
+			}
+			$name = explode(' ', $deviceModel);
+			if (!isset($name[0])) {
+				return;
+			}
+			$check = $name[0];
+			$check = strtoupper($check);
+			if ($check !== $name[0]) {
+				$player->close("", "Join with toolbox model='$deviceModel'");
+			}
+		}
+	}
 }
